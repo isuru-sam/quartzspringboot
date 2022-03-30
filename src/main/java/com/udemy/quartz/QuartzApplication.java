4eb614fb9576fcb6fd05955a93a6ae12f8dd7a97
ocbc01 <iisuru@gmail.com>
2022-03-30 21:05:06 +0530

    swag

--- src/main/java/com/udemy/quartz/QuartzApplication.java
+++ src/main/java/com/udemy/quartz/QuartzApplication.java
@@ -2,7 +2,9 @@ package com.udemy.quartz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @SpringBootApplication
+@EnableWebMvc
 public class QuartzApplication {
 
 	public static void main(String[] args) {
